55a9508e1c058125dbb994fdba8c53a784202eb4
qqri <cqo1154@gmail.com>
2023-07-06 22:17:50 +0900

    readme , memoapp

--- memoapp/lib/main.dart
+++ memoapp/lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:memoapp/screens/home.dart';
+import 'models/user_prefrences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await UserPrefrences.init();
+
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Text('안녕'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 37, 37, 37),
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
     );
-
   }
 }
